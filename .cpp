18eb7ddad723baabbcabb87d42500dfbaf769895
Giri231 <94457711+Giri231@users.noreply.github.com>
2022-09-10 00:03:06 +0530

    Create .cpp

---
+++ .cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+int sum(int x1,int x2)
+{
+    int c=x1+x2;
+    return c;
+}
+
+int main()
+{
+    int a,b;
+    cin>>a>>b;
+    cout<<sum(a,b);
+    
+    return 0;
+    
+}
